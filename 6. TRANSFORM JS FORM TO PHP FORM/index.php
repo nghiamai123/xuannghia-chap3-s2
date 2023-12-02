4f4d3eb0356b6c986a4ec3b4bbce59bf2c8c8b37
nghia <nghia.mai25@student.passerellesnumeriques.org>
2023-12-02 08:40:06 +0700

    DONE

--- 6. TRANSFORM JS FORM TO PHP FORM/index.php
+++ 6. TRANSFORM JS FORM TO PHP FORM/index.php
@@ -1,30 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Log in page</title>
-</head>
-<body>
-    <form>
-        <input type="text" name="password" id="password" placeholder="Password">
-        <button id="login_btn">Log in</button>
-    </form>
-
-    <div id="hidden" style="display:none">This is sensitive data visible only if the user knows the correct password</div>
-
-    <script>
-        function logIn(event) {
-        event.preventDefault();
-        let password = document.getElementById('password').value;
-            
-            if (password == <?= "1234"?>) {
-                document.getElementById('hidden').style.display = 'block';
-            }
+<?php 
+    if(isset($_POST["password"])){
+        if($_POST["password"] == "1234"){
+            echo '<script>document.getElementById("hidden").style.display = "block";</script>';
         }
-
-        document.getElementById('login_btn').addEventListener('click', logIn);
-    </script>
-</body>
-</html>
+    }
+?>
